b29e5793e6337a04e823cf27fcecfbc87d0fd7ba
clux <sszynrae@gmail.com>
2019-04-29 19:01:51 +0100

    cleanliness

--- src/state.rs
+++ src/state.rs
@@ -47,17 +47,14 @@ impl State {
         let foos = Reflector::new(client, fooresource)?;
         Ok(State { foos })
     }
-
     /// Internal poll for internal thread
     fn poll(&self) -> Result<()> {
         self.foos.poll()
     }
-
     /// Exposed refresh button for use by app
     pub fn refresh(&self) -> Result<()> {
         self.foos.refresh()
     }
-
     /// Exposed getter for read access to state for app
     pub fn foos(&self) -> Result<Cache<FooResource>> {
         self.foos.read()
@@ -69,20 +66,18 @@ impl State {
 /// This returns a `State` and calls `poll` on it continuously.
 /// As a result, this file encapsulates the only write access to a
 pub fn init(cfg: Configuration) -> Result<State> {
-    let client = APIClient::new(cfg);
-    let state = State::new(client)?; // for app to read
-    let state2 = state.clone(); // for internal thread to poll and update
+    let state = State::new(APIClient::new(cfg))?; // for app to read
+    let state_clone = state.clone(); // clone for internal thread
     std::thread::spawn(move || {
         loop {
             std::thread::sleep(Duration::from_secs(10));
-            // poll all reflectors here
-            // (this can cause a few more waits in edge cases)
-            match state2.poll() {
-                Ok(_) => trace!("State refreshed"),
+            // update state here - can cause a few more waits in edge cases
+            match state_clone.poll() {
+                Ok(_) => trace!("State refreshed"), // normal case
                 Err(e) => {
-                    // Bad fallback, but at least it leaves system working.
+                    // Can't recover: boot as much as kubernetes' backoff allows
                     error!("Failed to refesh cache '{}' - rebooting", e);
-                    std::process::exit(1);
+                    std::process::exit(1); // boot might fix it if network is failing
                 }
             }
         }
