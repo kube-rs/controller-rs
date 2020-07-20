f92766bb3719c3e6b0ef153f3ae27e1555952ef9
clux <sszynrae@gmail.com>
2020-07-20 16:01:22 +0100

    make it not build again

--- src/main.rs
+++ src/main.rs
@@ -37,13 +37,13 @@ async fn main() -> Result<()> {
     }
     env_logger::init();
     let client = kube::Client::try_default().await.expect("create client");
-    //let manager = Manager::new(client);
-    //let mfut = manager.run();
+    let manager = Manager::new(client);
+    let mfut = manager.run();
 
     let server = HttpServer::new(move || {
         App::new()
-            //.data(manager.clone())
-            .data(Manager::new(client.clone()))
+            .data(manager.clone())
+            //.data(Manager::new(client.clone()))
             .wrap(middleware::Logger::default().exclude("/health"))
             .service(index)
             .service(health)
@@ -55,7 +55,7 @@ async fn main() -> Result<()> {
     .run();
 
     tokio::select! {
-        //_ = mfut => warn!("controller drained"),
+        _ = mfut => warn!("controller drained"),
         _ = server => info!("actix exited"),
     }
     Ok(())
