c0600df813be5680757b52819ea5620d6e387891
Eirik A <sszynrae@gmail.com>
2022-11-30 08:43:33 +0000

    Factor `Metrics` out into its own file and use `Drop` for measurement (#32)
    * Factor metrics out into its own file and use Drop for measurement
    
    This avoids crowding the reconciler with error-prone specifics that can
    be handled more cleanly in a separate module.
    
    Signed-off-by: clux <sszynrae@gmail.com>
    
    * fix fmt
    
    Signed-off-by: clux <sszynrae@gmail.com>
    
    * better error handling and propagate errors to metrics
    
    Signed-off-by: clux <sszynrae@gmail.com>
    
    Signed-off-by: clux <sszynrae@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,27 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Finalizer Error: {0}")]
-    FinalizerError(#[source] kube::runtime::finalizer::Error<kube::Error>),
-
     #[error("SerializationError: {0}")]
     SerializationError(#[source] serde_json::Error),
+
+    #[error("Kube Error: {0}")]
+    KubeError(#[source] kube::Error),
+
+    #[error("Finalizer Error: {0}")]
+    // NB: awkward type because finalizer::Error embeds the reconciler error (which is this)
+    // so boxing this error to break cycles
+    FinalizerError(#[source] Box<kube::runtime::finalizer::Error<Error>>),
+
+    #[error("IllegalDocument")]
+    IllegalDocument,
 }
 pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+impl Error {
+    pub fn metric_label(&self) -> String {
+        format!("{:?}", self).to_lowercase()
+    }
+}
 
 /// State machinery for kube, as exposeable to actix
 pub mod manager;
@@ -19,3 +33,7 @@ pub use manager::Document;
 
 /// Log and trace integrations
 pub mod telemetry;
+
+/// Metrics
+mod metrics;
+pub use metrics::Metrics;
--- src/manager.rs
+++ src/manager.rs
@@ -1,4 +1,4 @@
-use crate::{telemetry, Error, Result};
+use crate::{telemetry, Error, Metrics, Result};
 use chrono::{DateTime, Utc};
 use futures::{future::BoxFuture, FutureExt, StreamExt};
 use kube::{
@@ -11,18 +11,11 @@ use kube::{
     },
     CustomResource, Resource,
 };
-use prometheus::{
-    default_registry, proto::MetricFamily, register_histogram_vec, register_int_counter, HistogramVec,
-    IntCounter,
-};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::sync::Arc;
-use tokio::{
-    sync::RwLock,
-    time::{Duration, Instant},
-};
+use tokio::{sync::RwLock, time::Duration};
 use tracing::*;
 
 static DOCUMENT_FINALIZER: &str = "documents.kube.rs";
@@ -38,7 +31,6 @@ pub struct DocumentSpec {
     hide: bool,
     content: String,
 }
-
 /// The status object of `Document`
 #[derive(Deserialize, Serialize, Clone, Debug, JsonSchema)]
 pub struct DocumentStatus {
@@ -66,40 +58,30 @@ struct Context {
 async fn reconcile(doc: Arc<Document>, ctx: Arc<Context>) -> Result<Action> {
     let trace_id = telemetry::get_trace_id();
     Span::current().record("trace_id", &field::display(&trace_id));
-    let start = Instant::now();
-    ctx.metrics.reconciliations.inc();
-    let client = ctx.client.clone();
-    let name = doc.name_any();
-    let ns = doc.namespace().unwrap();
-    let docs: Api<Document> = Api::namespaced(client, &ns);
+    ctx.metrics.count_and_measure();
+    let ns = doc.namespace().unwrap(); // doc is namespace scoped
+    let docs: Api<Document> = Api::namespaced(ctx.client.clone(), &ns);
 
-    let action = finalizer(&docs, DOCUMENT_FINALIZER, doc, |event| async {
+    info!("Reconciling Document \"{}\" in {}", doc.name_any(), ns);
+    finalizer(&docs, DOCUMENT_FINALIZER, doc, |event| async {
         match event {
             Finalizer::Apply(doc) => doc.reconcile(ctx.clone()).await,
             Finalizer::Cleanup(doc) => doc.cleanup(ctx.clone()).await,
         }
     })
     .await
-    .map_err(Error::FinalizerError);
-
-    let duration = start.elapsed().as_millis() as f64 / 1000.0;
-    ctx.metrics
-        .reconcile_duration
-        .with_label_values(&[])
-        .observe(duration);
-    info!("Reconciled Document \"{}\" in {}", name, ns);
-    action
+    .map_err(|e| Error::FinalizerError(Box::new(e)))
 }
 
-fn error_policy(_doc: Arc<Document>, error: &Error, ctx: Arc<Context>) -> Action {
+fn error_policy(doc: Arc<Document>, error: &Error, ctx: Arc<Context>) -> Action {
     warn!("reconcile failed: {:?}", error);
-    ctx.metrics.failures.inc();
+    ctx.metrics.reconcile_failure(&doc, error);
     Action::requeue(Duration::from_secs(5 * 60))
 }
 
 impl Document {
     // Reconcile (for non-finalizer related changes)
-    async fn reconcile(&self, ctx: Arc<Context>) -> Result<Action, kube::Error> {
+    async fn reconcile(&self, ctx: Arc<Context>) -> Result<Action> {
         let client = ctx.client.clone();
         ctx.diagnostics.write().await.last_event = Utc::now();
         let reporter = ctx.diagnostics.read().await.reporter.clone();
@@ -119,7 +101,11 @@ impl Document {
                     action: "Reconciling".into(),
                     secondary: None,
                 })
-                .await?;
+                .await
+                .map_err(Error::KubeError)?;
+        }
+        if name == "illegal" {
+            return Err(Error::IllegalDocument); // error names show up in metrics
         }
         // always overwrite status object with what we saw
         let new_status = Patch::Apply(json!({
@@ -130,14 +116,17 @@ impl Document {
             }
         }));
         let ps = PatchParams::apply("cntrlr").force();
-        let _o = docs.patch_status(&name, &ps, &new_status).await?;
+        let _o = docs
+            .patch_status(&name, &ps, &new_status)
+            .await
+            .map_err(Error::KubeError)?;
 
         // If no events were received, check back every 5 minutes
         Ok(Action::requeue(Duration::from_secs(5 * 60)))
     }
 
     // Reconcile with finalize cleanup (the object was deleted)
-    async fn cleanup(&self, ctx: Arc<Context>) -> Result<Action, kube::Error> {
+    async fn cleanup(&self, ctx: Arc<Context>) -> Result<Action> {
         let client = ctx.client.clone();
         ctx.diagnostics.write().await.last_event = Utc::now();
         let reporter = ctx.diagnostics.read().await.reporter.clone();
@@ -151,39 +140,10 @@ impl Document {
                 action: "Reconciling".into(),
                 secondary: None,
             })
-            .await?;
+            .await
+            .map_err(Error::KubeError)?;
 
         Ok(Action::await_change())
-    }
-}
-
-/// Prometheus Metrics to be exposed on /metrics
-#[derive(Clone)]
-pub struct Metrics {
-    pub reconciliations: IntCounter,
-    pub failures: IntCounter,
-    pub reconcile_duration: HistogramVec,
-}
-impl Metrics {
-    fn new() -> Self {
-        let reconcile_histogram = register_histogram_vec!(
-            "doc_controller_reconcile_duration_seconds",
-            "The duration of reconcile to complete in seconds",
-            &[],
-            vec![0.01, 0.1, 0.25, 0.5, 1., 5., 15., 60.]
-        )
-        .unwrap();
-
-        Metrics {
-            reconciliations: register_int_counter!("doc_controller_reconciliations_total", "reconciliations")
-                .unwrap(),
-            failures: register_int_counter!(
-                "doc_controller_reconciliation_errors_total",
-                "reconciliation errors"
-            )
-            .unwrap(),
-            reconcile_duration: reconcile_histogram,
-        }
     }
 }
 
@@ -195,8 +155,8 @@ pub struct Diagnostics {
     #[serde(skip)]
     pub reporter: Reporter,
 }
-impl Diagnostics {
-    fn new() -> Self {
+impl Default for Diagnostics {
+    fn default() -> Self {
         Self {
             last_event: Utc::now(),
             reporter: "doc-controller".into(),
@@ -205,7 +165,7 @@ impl Diagnostics {
 }
 
 /// Data owned by the Manager
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub struct Manager {
     /// Diagnostics populated by the reconciler
     diagnostics: Arc<RwLock<Diagnostics>>,
@@ -219,12 +179,11 @@ impl Manager {
     /// It is up to `main` to wait for the controller stream.
     pub async fn new() -> (Self, BoxFuture<'static, ()>) {
         let client = Client::try_default().await.expect("create client");
-        let metrics = Metrics::new();
-        let diagnostics = Arc::new(RwLock::new(Diagnostics::new()));
+        let manager = Manager::default();
         let context = Arc::new(Context {
             client: client.clone(),
-            metrics: metrics.clone(),
-            diagnostics: diagnostics.clone(),
+            metrics: Metrics::default(),
+            diagnostics: manager.diagnostics.clone(),
         });
 
         let docs = Api::<Document>::all(client);
@@ -241,12 +200,12 @@ impl Manager {
             .for_each(|_| futures::future::ready(()))
             .boxed();
 
-        (Self { diagnostics }, controller)
+        (manager, controller)
     }
 
     /// Metrics getter
-    pub fn metrics(&self) -> Vec<MetricFamily> {
-        default_registry().gather()
+    pub fn metrics(&self) -> Vec<prometheus::proto::MetricFamily> {
+        prometheus::default_registry().gather()
     }
 
     /// State getter
