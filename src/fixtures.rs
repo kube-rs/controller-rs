3d58a74c1f0ecbe812df3bbc08cb28c66ee8b999
Eirik A <sszynrae@gmail.com>
2024-04-03 17:33:22 +0100

    upgrade to 0.90 and use body-collect helper (#69)
    * showcase body-collect
    
    Signed-off-by: clux <sszynrae@gmail.com>
    
    * even simpler
    
    Signed-off-by: clux <sszynrae@gmail.com>
    
    * fix everywhere
    
    Signed-off-by: clux <sszynrae@gmail.com>
    
    * use main
    
    Signed-off-by: clux <sszynrae@gmail.com>
    
    * use official
    
    Signed-off-by: clux <sszynrae@gmail.com>
    
    ---------
    
    Signed-off-by: clux <sszynrae@gmail.com>

--- src/fixtures.rs
+++ src/fixtures.rs
@@ -2,7 +2,6 @@
 use crate::{Context, Document, DocumentSpec, DocumentStatus, Metrics, Result, DOCUMENT_FINALIZER};
 use assert_json_diff::assert_json_include;
 use http::{Request, Response};
-use http_body_util::BodyExt;
 use kube::{client::Body, Client, Resource, ResourceExt};
 use prometheus::Registry;
 use std::sync::Arc;
@@ -129,7 +128,7 @@ impl ApiServerVerifier {
             { "op": "test", "path": "/metadata/finalizers", "value": null },
             { "op": "add", "path": "/metadata/finalizers", "value": vec![DOCUMENT_FINALIZER] }
         ]);
-        let req_body = request.into_body().collect().await.unwrap().to_bytes();
+        let req_body = request.into_body().collect_bytes().await.unwrap();
         let runtime_patch: serde_json::Value =
             serde_json::from_slice(&req_body).expect("valid document from runtime");
         assert_json_include!(actual: runtime_patch, expected: expected_patch);
@@ -154,7 +153,7 @@ impl ApiServerVerifier {
             { "op": "test", "path": "/metadata/finalizers/0", "value": DOCUMENT_FINALIZER },
             { "op": "remove", "path": "/metadata/finalizers/0", "path": "/metadata/finalizers/0" }
         ]);
-        let req_body = request.into_body().collect().await.unwrap().to_bytes();
+        let req_body = request.into_body().collect_bytes().await.unwrap();
         let runtime_patch: serde_json::Value =
             serde_json::from_slice(&req_body).expect("valid document from runtime");
         assert_json_include!(actual: runtime_patch, expected: expected_patch);
@@ -172,7 +171,7 @@ impl ApiServerVerifier {
             format!("/apis/events.k8s.io/v1/namespaces/default/events?")
         );
         // verify the event reason matches the expected
-        let req_body = request.into_body().collect().await.unwrap().to_bytes();
+        let req_body = request.into_body().collect_bytes().await.unwrap();
         let postdata: serde_json::Value =
             serde_json::from_slice(&req_body).expect("valid event from runtime");
         dbg!("postdata for event: {}", postdata.clone());
@@ -195,7 +194,7 @@ impl ApiServerVerifier {
                 doc.name_any()
             )
         );
-        let req_body = request.into_body().collect().await.unwrap().to_bytes();
+        let req_body = request.into_body().collect_bytes().await.unwrap();
         let json: serde_json::Value = serde_json::from_slice(&req_body).expect("patch_status object is json");
         let status_json = json.get("status").expect("status object").clone();
         let status: DocumentStatus = serde_json::from_value(status_json).expect("valid status");
