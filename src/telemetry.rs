481b4975db4709c26ca269d9b525410d28fef3d2
clux <sszynrae@gmail.com>
2024-10-10 00:08:19 +0100

    build both features properly and fix build without the feature
    Signed-off-by: clux <sszynrae@gmail.com>

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,7 +1,6 @@
+#![allow(unused_imports)] // some used only for telemetry feature
 use opentelemetry::trace::{TraceId, TracerProvider};
-use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_sdk::{runtime, trace as sdktrace, trace::Config, Resource};
-use tracing_opentelemetry::OpenTelemetryLayer;
 use tracing_subscriber::{prelude::*, EnvFilter, Registry};
 
 ///  Fetch an opentelemetry::trace::TraceId as hex through the full tracing stack
@@ -16,6 +15,7 @@ pub fn get_trace_id() -> TraceId {
         .trace_id()
 }
 
+#[cfg(feature = "telemetry")]
 fn resource() -> Resource {
     use opentelemetry::KeyValue;
     Resource::new([
@@ -26,6 +26,7 @@ fn resource() -> Resource {
 
 #[cfg(feature = "telemetry")]
 fn init_tracer() -> sdktrace::Tracer {
+    use opentelemetry_otlp::WithExportConfig;
     let endpoint = std::env::var("OPENTELEMETRY_ENDPOINT_URL").expect("Needs an otel collector");
     let exporter = opentelemetry_otlp::new_exporter().tonic().with_endpoint(endpoint);
 
@@ -44,7 +45,7 @@ fn init_tracer() -> sdktrace::Tracer {
 pub async fn init() {
     // Setup tracing layers
     #[cfg(feature = "telemetry")]
-    let otel = OpenTelemetryLayer::new(init_tracer());
+    let otel = tracing_opentelemetry::OpenTelemetryLayer::new(init_tracer());
 
     let logger = tracing_subscriber::fmt::layer().compact();
     let env_filter = EnvFilter::try_from_default_env()
